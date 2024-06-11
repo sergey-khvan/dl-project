d1d573a01e8ec203305b89d80fcf2672dcee3e98
iegor-toporov <81853469+iegor-toporov@users.noreply.github.com>
2024-06-12 01:31:09 +0200

    Update on dataset.py adding data augmentation classes
    Added 3 classes to data augmentation: Random Mutations (does random mut.), ReverseComplement (substitutes with the complement), SequenceShuffle (takes a fixed size sequence and shuffles it).

--- train/dataset.py
+++ train/dataset.py
@@ -1,12 +1,44 @@
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+# THIS PART WAS MADE BY IEGOR
+import random
 
+class RandomMutation:
+    def __init__(self, mutation_rate=0.01):
+        self.mutation_rate = mutation_rate
+    
+    def __call__(self, sequence):
+        mutated_sequence = list(sequence)
+        for i in range(len(sequence)):
+            if random.random() < self.mutation_rate:
+                mutated_sequence[i] = random.choice('ACGT')
+        return ''.join(mutated_sequence)
+
+class ReverseComplement:
+    def __call__(self, sequence):
+        complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+        return ''.join(complement[base] for base in reversed(sequence))
+
+class SequenceShuffle:
+    def __init__(self, segment_size=10):
+        self.segment_size = segment_size
+    
+    def __call__(self, sequence):
+        segments = [sequence[i:i + self.segment_size] for i in range(0, len(sequence), self.segment_size)]
+        random.shuffle(segments)
+        return ''.join(segments)
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 class DNADataset(Dataset):
-    def __init__(self, csv_dir):
+    def __init__(self, csv_dir, transform=None):
         self.dataset = pd.read_csv(csv_dir, header=None)
         self.dataset = self.dataset.drop(0, axis=1)
+        # &&&&&&&&&&&&&&&&&&&&&&&&&&&
+        # THIS PART WAS MADE BY IEGOR
+        self.tranform = transform
+        # &&&&&&&&&&&&&&&&&&&&&&&&&&&
 
     def __len__(self):
         return len(self.dataset)
@@ -14,6 +46,11 @@ class DNADataset(Dataset):
     def __getitem__(self, idx):
         data = self.dataset.iloc[idx]
         code, label = data[1], data[2]
+        # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        # THIS PART WAS MADE BY IEGOR
+        if self.transform:
+            code = self.transform(code)
+        # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         code = self.encode(code)
         label = torch.Tensor([label])
         return code, label
